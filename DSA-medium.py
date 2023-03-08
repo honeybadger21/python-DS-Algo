92cc9da885965fbc2f405d473a955d40f4ec9ccd
Ruchi Sharma <statsnotes21@gmail.com>
2023-03-07 22:30:31 -0600

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -364,6 +364,63 @@ class Solution:
 # Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 # A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
 
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+     
+        # Brute Force
+        hash1 = {
+            '2':['a', 'b', 'c'],
+            '3':['d', 'e', 'f'],
+            '4':['g', 'h', 'i'],
+            '5':['j', 'k', 'l'],
+            '6':['m', 'n', 'o'],
+            '7':['p', 'q', 'r', 's'],
+            '8':['t', 'u', 'v'],
+            '9':['w', 'x', 'y', 'z']
+        }
+           
+        arr = []
+        for elem in digits:
+            arr.append(hash1[elem])
+
+        if arr == []:
+            return arr
+
+        res = arr[0]
+        fin = []
+        for i in range(1, len(arr)):
+            for elem1 in res:
+                for elem2 in arr[i]:
+                    temp = elem1+elem2
+                    fin.append(temp)
+                res = fin
+
+        return res
+      
+        # Python Library based Solution
+        return list(map(''.join, product(*({'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}[digit] for digit in digits)))) if digits else []
+       
+        
+        # Hashmap + Backtracking Solution
+        res = []
+        def backtrack(i, curstrng):
+            if len(curstrng) == len(digits):
+                res.append(curstrng)
+                return
+
+            for c in hash1[digits[i]]:
+                backtrack(i+1, curstrng+c)
+            
+        if digits:
+            backtrack(0, "")
+
+        return res
+
+
+
+ 
+
+
 
             
 
