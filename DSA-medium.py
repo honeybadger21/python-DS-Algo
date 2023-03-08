e3d9503ad891c8460e33447a5e97130864b9cd78
Ruchi Sharma <statsnotes21@gmail.com>
2023-03-07 21:00:44 -0600

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -323,7 +323,22 @@ class Solution:
                         ans = s[i:j+1]
         return ans
 
-    # 07. Reverse Integer
+# 07. Reverse Integer
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        neg = 0
+        x = str(x)
+        if x[0] == "-":
+            neg = 1
+            x = x[1:]
+        x = x[::-1]
+        x = int(x)
+        if neg == 1:
+            x = -x
+        return x if (x >= -2**31) and (x <= (2**31)-1) else 0
     
 
         
