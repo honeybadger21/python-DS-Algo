5f4218988b67ed3c5c98e37c9b4d87c6a1d69cf4
Ruchi Sharma <statsnotes21@gmail.com>
2023-03-15 02:22:21 -0500

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -580,10 +580,33 @@ class Solution:
             return True
         return False
 
+# 102. Binary Tree Level Order Traversal
+# Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
 
-
-
+        queue, result = [], []
+        if root:
+            queue.append(root)
+        while queue:
+            size=len(queue)
+            level=[]
+            for i in range(size):
+                node=queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)   
+            result.append(level)
+        return result   
 
 
 
