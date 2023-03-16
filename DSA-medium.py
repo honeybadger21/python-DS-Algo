8154d4686f24a0a342c63be2e7042845a9e3655c
Ruchi Sharma <statsnotes21@gmail.com>
2023-03-16 03:51:12 -0500

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -660,8 +660,49 @@ class Solution:
         arr = inorderTraversal(root)
         return arr[k-1]
 
-
+# 236. Lowest Common Ancestor of a Binary Tree
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
     
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
+        # stack for tree traversal
+        stack=[root]
 
+        # dictionary for parent pointers
+        parent = {root:None}
+
+        # iterate until we find both the nodes p and q 
+        while p not in parent or q not in parent:
+            node=stack.pop()
+            # while traversing the tree, keep saving the parent pointers
+            if node.left:
+                parent[node.left]=node
+                stack.append(node.left)
+            if node.right:
+                parent[node.right]=node
+                stack.append(node.right)
+
+        # ancestors set() for node p 
+        ancestors = set()
+
+        # process all ancestors for node p using parent pointers 
+        while p:
+            ancestors.add(p)
+            p=parent[p]
+        
+        # the first ancestor of q which appears in p's ancestor set() is their LCA
+        while q not in ancestors:
+            q=parent[q]
+        
+        return q
+
+            
