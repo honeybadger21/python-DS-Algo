ac1b56a695e33c39ce2ea2621deb758a21123b3b
Ruchi Sharma <statsnotes21@gmail.com>
2023-04-15 17:18:32 -0500

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -855,7 +855,33 @@ class Solution:
 
         return True
 
+# 38. Count and Say
+# The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+# countAndSay(1) = "1"
+# countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
 
+# To determine how you "say" a digit string, split it into the minimal number of substrings such that each substring contains exactly one unique digit. 
+# Then for each substring, say the number of digits, then say the digit. Finally, concatenate every said digit.
+    
+## I don 't understand the question clearly ##
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+
+        if n==1:
+            return "1"
+        prev=self.countAndSay(n-1)
+        count,res=1,""
+        for i in range(len(prev)):
+            if i==len(prev)-1 or prev[i]!=prev[i+1]:
+                res+=str(count)
+                res+=prev[i]
+                count=1
+            else:
+                count+=1
+        return res
         
+     
+     
 
 
