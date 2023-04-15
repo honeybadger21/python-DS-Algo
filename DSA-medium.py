900af54786a3bb25c5b13b0b5942f99dc128f226
Ruchi Sharma <statsnotes21@gmail.com>
2023-04-15 18:41:05 -0500

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -984,7 +984,26 @@ class Solution:
 	        for j in range(i):
 		        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 		
+# 54. Spiral Matrix
+# Given an m x n matrix, return all elements of the matrix in spiral order.
 
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ans = []
+
+        while matrix:
+            ans += matrix.pop(0)
+
+            if matrix and matrix[0]:
+                for line in matrix:
+                    ans.append(line.pop())
+            if matrix:
+                ans+=matrix.pop()[::-1]
+
+            if matrix and matrix[0]:
+                for line in matrix[::-1]:
+                    ans.append(line.pop(0))
+        return ans
 
      
      
