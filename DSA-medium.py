1724a4b23f3b6425ac633e57b452b49af582b245
Ruchi Sharma <statsnotes21@gmail.com>
2023-04-16 02:00:45 -0500

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -1005,7 +1005,16 @@ class Solution:
                     ans.append(line.pop(0))
         return ans
 
-     
-     
+# 55. Jump Game
+# You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+# Return true if you can reach the last index, or false otherwise.
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last_position = len(nums)-1
+        for i in range(len(nums)-2, -1, -1):
+            if i+nums[i] >= last_position:
+                last_position = i
+        return last_position == 0
 
 
