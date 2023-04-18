a410f758c02ada1c0630b3bb562c700691a8cc72
Ruchi Sharma <statsnotes21@gmail.com>
2023-04-18 16:48:58 -0500

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -1092,6 +1092,30 @@ class Solution:
         node.val=node.next.val
         node.next=node.next.next
   
+# 91. Decode Ways
+# https://leetcode.com/problems/decode-ways/description/
+# Very tough for me to wrap my head around this problem!!!!!!!!!!
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        if len(s) < 1:
+            return 1
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+
+        if s[0]=="0":
+            dp[1] = 0
+        else:
+            dp[1] = 1
+        
+        for i in range(2, len(s)+1):
+            if 1 <= int(s[i-1 : i]) <=10:
+                dp[i] += dp[i-1]
+            if 10 <= int(s[i-2 : i]) <= 26:
+                dp[i] += dp[i-2] 
+        # print(dp)
+        return dp[-1]
         
         
 
