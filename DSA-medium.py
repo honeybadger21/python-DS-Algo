31cb52d1592840c6be080902b7b51d25e338961a
Ruchi Sharma <statsnotes21@gmail.com>
2023-04-22 14:21:21 -0500

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -1198,3 +1198,20 @@ class Solution:
 
         return 0
 
+# 300. Longest Increasing Subsequence
+# Given an integer array nums, return the length of the longest strictly increasing subsequence. 
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [0]*(len(nums))
+        dp[0] = 1
+
+        for i in range(1, len(nums)):
+            maxi = 0
+
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    maxi = max(maxi, dp[j])
+            dp[i] = 1+maxi
+
+        return max(dp)     
