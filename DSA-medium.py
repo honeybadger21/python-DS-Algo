caf853435e5a3ca545c050fc836f24906462b7e5
Ruchi Sharma <statsnotes21@gmail.com>
2023-04-22 16:50:21 -0500

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -1321,3 +1321,42 @@ class Solution:
         node.random = self.copyRandomList(head.random)
 
         return node
+
+# 146. LRU Cache
+# Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+# Implement the LRUCache class:
+# LRUCache(int capacity) --> Initialize the LRU cache with positive size capacity.
+# int get(int key) --> Return the value of the key if the key exists, otherwise return -1.
+# void put(int key, int value) --> Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+# The functions get and put must each run in O(1) average time complexity.
+
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.odict=OrderedDict()
+        self.capacity=capacity
+        
+    def get(self, key: int) -> int:
+        if key not in self.odict:
+            return -1
+        else:
+            self.odict.move_to_end(key)
+            return self.odict[key]
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.odict:
+            self.odict[key]=value
+            self.odict.move_to_end(key)
+        else:
+            self.odict[key]=value
+            if len(self.odict)>self.capacity:
+                self.odict.popitem(last=False)
+            
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+
