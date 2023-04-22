1eafc847f8655e388e1f28ec639b9f6457acaf6b
Ruchi Sharma <statsnotes21@gmail.com>
2023-04-22 16:11:08 -0500

    Update DSA-medium.py

--- DSA-medium.py
+++ DSA-medium.py
@@ -1253,3 +1253,32 @@ class Solution:
                 return res[0] + '.' + ''.join(res[1:])
 
         return res[0] + '.' + ''.join(res[1:remainders[remainder] + 1]) + '(' + ''.join(res[remainders[remainder] + 1:]) + ')'
+
+# 328. Odd Even Linked List
+# Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+# The first node is considered odd, and the second node is even, and so on.
+# Note that the relative order inside both the even and odd groups should remain as it was in the input.
+# You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        odd = head
+
+        if head is None:
+            return head
+        
+        even = head.next
+        evenhead = head.next
+
+        while even and even.next is not None:
+            odd.next = odd.next.next
+            odd = odd.next 
+            even.next = even.next.next
+            even = even.next
+        odd.next = evenhead
+        return head
