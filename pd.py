01e22e688886cf9bcf1eefd1313213981e663368
Ruchi Sharma <statsnotes21@gmail.com>
2024-06-25 00:28:08 -0500

    Update pd.py

--- pd.py
+++ pd.py
@@ -3,3 +3,8 @@ def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     df = pd.DataFrame(student_data)
     df.columns=['student_id', 'age']
     return df
+
+# 2878. Get the Size of a DataFrame
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return list(players.shape)
+
