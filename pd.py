bf123abf139a3c46a0371b0cf3dc42f4f82d79e2
Ruchi Sharma <statsnotes21@gmail.com>
2024-06-25 00:26:11 -0500

    Update pd.py

--- pd.py
+++ pd.py
@@ -1,1 +1,5 @@
-
+# 2877. Create a DataFrame from List
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    df = pd.DataFrame(student_data)
+    df.columns=['student_id', 'age']
+    return df
